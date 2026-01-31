dfd27b05f899fe7bab5ae45b041661abf8486251
Anand Sundar <anandsundar96@gmail.com>
2026-01-31 15:45:06 -0700

    fixed typos in imports in main.go

--- store/memory_test.go
+++ store/memory_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/AnandSundar/go-idempotency"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/yourusername/go-idempotency"
 )
 
 func TestMemoryStore_SetAndGet(t *testing.T) {
